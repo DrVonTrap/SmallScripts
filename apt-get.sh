90481b4f57c9855ab26c5231caa159e55c1728c3
Freddie Ridell <DrVonTrap@gmail.com>
2015-07-26 14:36:46 +0100

    broke up apt-get.sh into multipul lines

--- apt-get.sh
+++ apt-get.sh
@@ -1,3 +1,5 @@
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y -m vim zsh byobu make cmake git firefox g++ gcc sublime-text qbittorrent clementine dropbox
+sudo apt-get install -y -m vim zsh byobu make cmake git firefox g++ gcc curl gparted feh htop nload nethogs
+sudo apt-get install -y -m qbittorrent clementine dropbox i3lock xautolock scrot convert composite
+sudo apt-get instal -y -m freeglut3-dev libglew-dev libglm-dev
