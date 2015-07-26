5878940ca450268e6db1672368be58e3f24d2181
Freddie Ridell <DrVonTrap@gmail.com>
2015-07-26 15:42:40 +0100

    changed install method and commented

--- apt-get.sh
+++ apt-get.sh
@@ -1,5 +1,10 @@
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y -m vim zsh byobu make cmake git firefox g++ gcc curl gparted feh htop nload nethogs
-sudo apt-get install -y -m qbittorrent clementine dropbox i3lock xautolock scrot convert composite
-sudo apt-get instal -y -m freeglut3-dev libglew-dev libglm-dev
+for i in package1 package2 package3; do sudo apt-get install -y $i; done
+
+#essentials
+for p in vim zsh byobu make cmake git firefox g++ gcc curl gparted feh htop nload nethogs pip; do sudo apt-get install -y $p; done
+#home PC
+for p in qbittorrent clementine dropbox i3lock xautolock scrot convert composite; do sudo apt-get install -y $p; done
+#OpenGL
+for p in freeglut3-dev libglew-dev libglm-dev; do sudo apt-get install -u $p; done
