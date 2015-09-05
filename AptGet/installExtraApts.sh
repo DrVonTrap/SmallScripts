2e646d0bf39662d1bd2037d1454f29296e9e5cff
Freddie Ridell <DrVonTrap@gmail.com>
2015-09-05 11:46:32 +0100

    yo

--- AptGet/installExtraApts.sh
+++ AptGet/installExtraApts.sh
@@ -1,3 +1,9 @@
+#syncthing:
 curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
 echo deb http://apt.syncthing.net/ syncthing release | sudo tee /etc/apt/sources.list.d/syncthing-release.list
+
+#llvm
 wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
+
+#variety wp changer
+sudo add-apt-repository ppa:peterlevi/ppa
