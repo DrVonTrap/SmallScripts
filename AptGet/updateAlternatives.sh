7bc0296b4bef65a805a8a0baf42da48ba4887d37
Freddie Ridell <DrVonTrap@gmail.com>
2015-09-05 19:08:35 +0100

    yo

--- AptGet/updateAlternatives.sh
+++ AptGet/updateAlternatives.sh
@@ -1,5 +1,2 @@
-sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.6 60 
-sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.6 60
-sudo update-alternatives --install /usr/bin/lldb lldb /usr/bin/lldb-3.6 60
-sudo update-alternatives --config clang
+sudo update-alternatives --install /usr/bin/lldb lldb /usr/bin/lldb-3.4 60
 sudo update-alternatives --config lldb
