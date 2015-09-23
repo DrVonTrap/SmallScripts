e526490d3ef39d6bd73d96818372850a5a10be56
Freddie Ridell <DrVonTrap@gmail.com>
2015-09-23 22:49:52 +0100

    yo

---
+++ i3GapsInstall.sh
@@ -0,0 +1,9 @@
+cd Documents
+git clone git@github.com:Airblader/i3.git
+cd i3
+git checkout gaps && git pull
+
+sudo apt-get install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev
+
+make
+sudo make install
